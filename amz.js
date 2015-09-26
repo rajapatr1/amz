947634232b60804ad98c1b19dcb4f676c2253a89
Rajapatr <mostac129@gmail.com>
2015-09-26 21:21:17 +0700

    Create amz.js

---
+++ amz.js
@@ -0,0 +1,10 @@
+function mosttk(tempe,dele) {
+  return document.location.href="http://www.mostmalls.com/store/?p="+tempe;
+}
+
+function most1(tempe,dele) {
+  return document.location.href="http://www.mostmalls.com/store/?p="+tempe;
+}
+function mostl(tempe,dele) {
+  return document.location.href="http://www.mostmalls.com/store/?p="+tempe;
+}

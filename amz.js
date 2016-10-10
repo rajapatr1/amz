e5bd4ce76025f49f050c923a48dc39d27a7589df
Rajapatr <mostac129@gmail.com>
2016-10-10 23:44:59 +0700

    Update amz.js

--- amz.js
+++ amz.js
@@ -1,13 +1,14 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
-function mosttk(tempe,dele) {
-  return document.location.href="http://www.mostmalls.com/l/"+tempe;
-}
 
-function most1(tempe,dele) {
-  return document.location.href="http://www.mostmalls.com/l/"+tempe/;
-}
-function mostl(tempe,dele) {
-  return document.location.href="http://www.mostmalls.com/l/"+tempe/;
-}
+	function mosttk(tempe,dele) {
+	  return document.location.href="http://www.mostmalls.com/l/"+tempe;
+	}
+
+	function most1(tempe,dele) {
+	  return document.location.href="http://www.mostmalls.com/l/"+tempe/;
+	}
+	function mostl(tempe,dele) {
+	  return document.location.href="http://www.mostmalls.com/l/"+tempe/;
+	}
+
 });

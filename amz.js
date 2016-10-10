3c79ead19916e0bb171ad9787fddfb0532ba1869
Rajapatr <mostac129@gmail.com>
2016-10-10 23:43:20 +0700

    Update amz.js

--- amz.js
+++ amz.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-});
 function mosttk(tempe,dele) {
   return document.location.href="http://www.mostmalls.com/l/"+tempe;
 }
@@ -11,3 +10,4 @@ function most1(tempe,dele) {
 function mostl(tempe,dele) {
   return document.location.href="http://www.mostmalls.com/l/"+tempe/;
 }
+});

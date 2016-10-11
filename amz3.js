d04c670b357204c15300be35c03d81bdab863eee
Rajapatr <mostac129@gmail.com>
2016-10-11 13:01:25 +0700

    Create amz3.js

---
+++ amz3.js
@@ -0,0 +1,6 @@
+ function mosttk(tempe,dele) {
+window.location.href = "http://www.mostmalls.com/l/"+tempe;
+	return false;
+  window.event.returnValue = false;
+   // return window.location.assign("SearchResults.aspx?search=" + txtBoxValue);
+}

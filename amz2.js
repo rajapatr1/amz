5a9bba569812c6793f34d00ba37042c5b28370b6
Rajapatr <mostac129@gmail.com>
2016-10-11 12:58:45 +0700

    Update amz2.js

--- amz2.js
+++ amz2.js
@@ -1,7 +1,7 @@
  function mosttk(tempe,dele) {
    // return window.location = "http://www.mostmalls.com/l/"+tempe;
 	// return window.location = 'http://www.google.com';
-	return window.location.assign(
+	return window.window.open(
   "http://www.mostmalls.com/l/"+tempe,'_blank');
    // return window.location.assign("SearchResults.aspx?search=" + txtBoxValue);
 }

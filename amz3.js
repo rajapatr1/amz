3195680a89b95377e537e8b545524860579b6226
Rajapatr <mostac129@gmail.com>
2017-05-06 18:34:50 +0700

    site change

--- amz3.js
+++ amz3.js
@@ -1,5 +1,5 @@
  function mosttk(tempe,dele) {
-window.location.href = "http://www.mostmalls.com/l/"+tempe;
+window.location.href = "http://www.shocksandstruts.info/l/"+tempe;
 	return false;
   window.event.returnValue = false;
    // return window.location.assign("SearchResults.aspx?search=" + txtBoxValue);

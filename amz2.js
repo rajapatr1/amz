af32334ce1c513a44e67f2b83cb2ab5436f8c405
Rajapatr <mostac129@gmail.com>
2017-05-06 18:34:33 +0700

    changesite

--- amz2.js
+++ amz2.js
@@ -2,6 +2,6 @@
    // return window.location = "http://www.mostmalls.com/l/"+tempe;
 	// return window.location = 'http://www.google.com';
 	return window.window.open(
-  "http://www.mostmalls.com/l/"+tempe,'_blank');
+  "http://www.shocksandstruts.info/l/"+tempe,'_blank');
    // return window.location.assign("SearchResults.aspx?search=" + txtBoxValue);
 }

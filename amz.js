93b712a4671047ab5994dda2950368d4b8484f52
Rajapatr <mostac129@gmail.com>
2017-05-06 18:34:08 +0700

    Update amz.js

--- amz.js
+++ amz.js
@@ -1,10 +1,10 @@
 function mosttk(tempe,dele) {
-	  return document.location.href="http://www.mostmalls.com/l/"+tempe;
+	  return document.location.href="http://www.shocksandstruts.info/l/"+tempe;
 	}
 	function most1(tempe,dele) {
-	  return document.location.href="http://www.mostmalls.com/l/"+tempe;
+	  return document.location.href="http://www.shocksandstruts.info/l/"+tempe;
 	}
 	function mostl(tempe,dele) {
-	  return document.location.href="http://www.mostmalls.com/l/"+tempe;
+	  return document.location.href="http://www.shocksandstruts.info/l/"+tempe;
 	}
 
